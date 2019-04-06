e041c10c57d8f22ef11346f223d80c4e2061e840
BryGuy <brnunn00@gmail.com>
2019-04-06 16:56:10 -0700

    some user presence added

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,7 @@ var playerdbid = '';
 var playerNameGl = '';
 var seatoneUser = '';
 var seattwoUser = '';
-
+var connected  = false;
 function loadUserData() {
     let id = localStorage.userId;
     if (id) {
@@ -22,20 +22,22 @@ function loadUserData() {
         if (name) {
             alert("Welcome back, " + name);
             $("#welcomeName").text(name + ", ")
-            database.ref(`GameOne/users/${id}`).once("value", snapshot => {
-                if (snapshot.exists()) {
-                    //const userData = snapshot.val();
-                    console.log("Player Found");
-                    $("#nameSub").css("display", "none");
-                    $("#nameEnter").css("display", "none");
-                    $("#nameLab").css("display", "none");
-                    playerdbid = id;
-                    playerNameGl = name;
-                } else {
+            // database.ref(`GameOne/users/${id}`).once("value", snapshot => {
+            //     if (snapshot.exists()) {
+            //         //const userData = snapshot.val();
+            //         console.log("Player Found");                   
+                    //playerdbid = id;
+                    // playerNameGl = name;
+                // } else {
                     popDb(name);
-                }
-            });
+                // }
+                initializeSeats();
+            // });
+            $("#nameSub").css("display", "none");
+            $("#nameEnter").css("display", "none");
+            $("#nameLab").css("display", "none");
         }
+       
     }
 }
 
@@ -64,6 +66,7 @@ function addNewUserToList() {
                 $("#nameSub").css("display", "none");
                 $("#nameEnter").css("display", "none");
                 $("#nameLab").css("display", "none");
+                initializeSeats();
             } else {
                 Swal.fire("Ok submit when ready");
             }
@@ -76,6 +79,8 @@ function addNewUserToList() {
 
 }
 function popDb(name) {
+
+
     var newUser = database.ref("GameOne/users").push({
         name: name,
         table: '',
@@ -87,7 +92,25 @@ function popDb(name) {
     localStorage.setItem("name", name);
     playerNameGl = name;
     playerdbid = newUser.key;
+
+    var connectionsRef = database.ref(`GameOne/users/${playerdbid}/connections`);
+  var connectedRef = database.ref(".info/connected");
+  connectedRef.on("value", function(snap) {
+
+      // If they are connected..
+      if (snap.val()) {
+    
+        // Add user to the connections list.
+        var con = connectionsRef.push(true);
+    
+        // Remove user from the connection list when they disconnect.
+        con.onDisconnect().remove();
+
+      }
+    });
+  
 }
+
 function addToWatcher(uKey, uName) {
     let nameEle = $("<li>");
     nameEle.attr("data-id", uKey);
@@ -95,17 +118,25 @@ function addToWatcher(uKey, uName) {
     nameEle.text(uName);
 
     $("#nameList").append(nameEle);
-
-
-
-
 }
 
 function checkSeat() {
     let ele = $(this);
+
+    console.log(ele);
     console.log(ele.data("playerid"));
-    let playerSitting = ele.data("playerid");
-    if (playerSitting) {
+    let playerSitting = ele.attr("data-playerid");
+    if (playerSitting == playerdbid){
+        let seatNum = ele.attr("data-seatnum");
+        database.ref("GameOne/TableOne/"+seatNum).remove();
+               
+      if (seatNum =="SeatOne"){
+            seatoneUser = ""
+      } else {
+        seattwoUser = ""
+      }
+    }
+    else if (playerSitting) {
         alert("Someone is already sitting there!");
     } else {
         console.log("Empty");
@@ -118,12 +149,44 @@ function sitInSeat(ele) {
     let seatNum = ele.data("seatnum");
 
     console.log(seatNum);
+
     database.ref(`GameOne/TableOne/${seatNum}`).push({
         id: playerdbid,
         name: playerNameGl,
-        seat: "one"
+        seat: seatNum
+    },function(){
+    if (seatoneUser != '' && seattwoUser != ''){
+
+    }
     })
+
 }
+
+
+  // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+  
+  database.ref(`GameOne/users/${playerdbid}`).on("child_removed", function(snapshot){
+      console.log(snapshot.val().seat);
+if (seatoneUser == playerdbid){
+
+    seatoneUser = '';
+} else if( seattwoUser == playerdbid){
+
+    // haltGame();
+}
+})
+
+database.ref("GameOne/TableOne/SeatOne").on("child_removed", function(snapshot){
+    let ele = $("#p1t1");
+    ele.text("Click to Join");
+    ele.attr("data-playerid",'');
+})
+database.ref("GameOne/TableOne/SeatTwo").on("child_removed", function(snapshot){
+   
+    let ele = $("#p2t1");
+    ele.text("Click to Join");
+       ele.attr("data-playerid",'');
+})
 
 function initializeSeats() {
 
@@ -139,24 +202,47 @@ $(document).ready(function () {
     })
     database.ref("GameOne/TableOne/SeatOne").on('child_added', function (snapshot) {
         if (snapshot.val().name != undefined) {
+            let sitterId  = snapshot.val().id;
             console.log(snapshot.val())
             $("#p1t1").text(snapshot.val().name);
             console.log(snapshot.val().id);
-            $("#p1t1").attr("data-playerid", snapshot.val().id);
-            seatoneUser = snapshot.val().id;
+            $("#p1t1").attr("data-playerid",sitterId);
+            seatoneUser = sitterId;
+            if (seattwoUser == seatoneUser){
+                database.ref("GameOne/TableOne").child("SeatTwo").remove();
+               let emptySeat = $("#p2t1")
+               emptySeat.text("Click to Join");
+               emptySeat.attr("data-playerid",'');
+               seattwoUser = ''
+
+            }
+            database.ref(`GameOne/users/${sitterId}`).update({
+                seat:"SeatOne"
+            })
         }
 
     })
     database.ref("GameOne/TableOne/SeatTwo").on('child_added', function (snapshot) {
         if (snapshot.val().name != undefined) {
+            let sitterId  = snapshot.val().id;
             $("#p2t1").text(snapshot.val().name);
-            $("#p2t1").attr("data-playerid", snapshot.val().id);
-            seattwoUser = snapshot.val().id;
+            $("#p2t1").attr("data-playerid", sitterId);
+            seattwoUser = sitterId;
+            if (seattwoUser == seatoneUser){
+                database.ref("GameOne/TableOne").child("SeatOne").remove();
+                let emptySeat = $("#p1t1")
+               emptySeat.text("Click to Join");
+               emptySeat.attr("data-playerid",'');
+               seatoneUser= '';
+            }
+            database.ref(`GameOne/users/${sitterId}`).update({
+                seat:"SeatTwo"
+            })
         }
 
     })
     loadUserData();
-    initializeSeats();
+   
     $('#nameSub').on('click', addNewUserToList);
     $(document.body).on('click', ".playerSeat", checkSeat);
 });
